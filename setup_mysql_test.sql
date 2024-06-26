ef8ec2e9e6396cdaf0a50100225af987fc563380
Gadoskey <gadoski321@gmail.com>
2024-06-27 00:11:46 +0100

    4-still

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -7,9 +7,6 @@ CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
 -- Grant all privileges on the database hbnb_test_db to hbnb_test
 GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
 
--- Revoke all privileges on other databases for hbnb_test (to ensure specific privileges)
-REVOKE ALL PRIVILEGES ON *.* FROM 'hbnb_test'@'localhost';
-
 -- Grant SELECT privilege on the performance_schema database to hbnb_test
 GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
 
