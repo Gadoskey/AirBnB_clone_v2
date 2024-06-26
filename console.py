820e872c7227142cbe8ff84e7c85b0406aa2d8ff
gadoskey <gadoski@example.com>
2024-06-26 08:12:11 -0700

    2

--- console.py
+++ console.py
@@ -73,7 +73,7 @@ class HBNBCommand(cmd.Cmd):
                 pline = pline[2].strip()  # pline is now str
                 if pline:
                     # check for *args or **kwargs
-                    if pline[0] is '{' and pline[-1] is'}'\
+                    if pline[0] == '{' and pline[-1] == '}'\
                             and type(eval(pline)) is dict:
                         _args = pline
                     else:
@@ -125,44 +125,45 @@ class HBNBCommand(cmd.Cmd):
         storage.save()
         print(new_instance.id)
         storage.save()"""
+
     def do_create(self, arg):
         """ Create an object of any class with given parameters """
-    if not arg:
-        print("** class name missing **")
-        return
+        if not arg:
+            print("** class name missing **")
+            return
+        
+        args_list = arg.split()
+        class_name = args_list[0]
 
-    args_list = arg.split()
-    class_name = args_list[0]
+        if class_name not in HBNBCommand.classes:
+            print("** class doesn't exist **")
+            return
 
-    if class_name not in HBNBCommand.classes:
-        print("** class doesn't exist **")
-        return
+        new_instance = HBNBCommand.classes[class_name]()
 
-    new_instance = HBNBCommand.classes[class_name]()
-
-    for param in args_list[1:]:
-        if '=' not in param:
-            continue
-
-        key, value = param.split('=', 1)
-
-        if value.startswith('"') and value.endswith('"'):
-            value = value[1:-1].replace('\\"', '"').replace('_', ' ')
-        elif '.' in value:
-            try:
-                value = float(value)
-            except ValueError:
-                continue
-        else:
-            try:
-                value = int(value)
-            except ValueError:
+        for param in args_list[1:]:
+            if '=' not in param:
                 continue
 
-        setattr(new_instance, key, value)
+            key, value = param.split('=', 1)
 
-    new_instance.save()
-    print(new_instance.id)
+            if value.startswith('"') and value.endswith('"'):
+                value = value[1:-1].replace('\\"', '"').replace('_', ' ')
+            elif '.' in value:
+                try:
+                    value = float(value)
+                except ValueError:
+                    continue
+            else:
+                try:
+                    value = int(value)
+                except ValueError:
+                    continue
+
+            setattr(new_instance, key, value)
+
+        new_instance.save()
+        print(new_instance.id)
 
 
     def help_create(self):
@@ -311,7 +312,7 @@ class HBNBCommand(cmd.Cmd):
                 args.append(v)
         else:  # isolate args
             args = args[2]
-            if args and args[0] is '\"':  # check for quoted arg
+            if args and args[0] == '\"':  # check for quoted arg
                 second_quote = args.find('\"', 1)
                 att_name = args[1:second_quote]
                 args = args[second_quote + 1:]
@@ -319,10 +320,10 @@ class HBNBCommand(cmd.Cmd):
             args = args.partition(' ')
 
             # if att_name was not quoted arg
-            if not att_name and args[0] is not ' ':
+            if not att_name and args[0] != ' ':
                 att_name = args[0]
             # check for quoted val arg
-            if args[2] and args[2][0] is '\"':
+            if args[2] and args[2][0] == '\"':
                 att_val = args[2][1:args[2].find('\"', 1)]
 
             # if att_val was not quoted arg
