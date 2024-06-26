867191a1fceb3722053e3e66498b44981b18abca
Gadoskey <gadoski321@gmail.com>
2024-06-27 00:04:39 +0100

    4

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -7,9 +7,11 @@ CREATE USER
 -- Grant privileges
 GRANT ALL PRIVILEGES
    ON `hbnb_test_db`.*
-   TO 'hbnb_test'@'localhost';
+   TO 'hbnb_test'@'localhost'
+   IDENTIFIED BY 'hbnb_test_pwd';
 GRANT SELECT
    ON `performance_schema`.*
-   TO 'hbnb_test'@'localhost';
+   TO 'hbnb_test'@'localhost'
+   IDENTIFIED BY 'hbnb_test_pwd';
 --Flush to apply changes
 FLUSH PRIVILEGES;
