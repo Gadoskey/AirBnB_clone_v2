aad0376a562e87a913261b39a2749f618bbb4af8
gadoskey <gadoski@example.com>
2024-06-29 14:08:55 -0700

    fffff

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,14 +1,17 @@
--- Create the database hbnb_test_db if it does not exist
+-- Create Database if not exist
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
-
--- Create the user hbnb_test with password hbnb_test_pwd if it does not exist
-CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
-
--- Grant all privileges on the database hbnb_test_db to hbnb_test
-GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
-
--- Grant SELECT privilege on the performance_schema database to hbnb_test
-GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
-
--- Flush privileges to apply changes
+--Create User if not exist
+CREATE USER
+    IF NOT EXISTS 'hbnb_test'@'localhost'
+    IDENTIFIED BY 'hbnb_test_pwd';
+-- Grant privileges
+GRANT ALL PRIVILEGES
+   ON `hbnb_test_db`.*
+   TO 'hbnb_test'@'localhost'
+   IDENTIFIED BY 'hbnb_test_pwd';
+GRANT SELECT
+   ON `performance_schema`.*
+   TO 'hbnb_test'@'localhost'
+   IDENTIFIED BY 'hbnb_test_pwd';
+--Flush to apply changes
 FLUSH PRIVILEGES;
