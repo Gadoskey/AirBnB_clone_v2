4a75fab2134b9d3443895c65ef605b84eedb920b
gadoskey <gadoski@example.com>
2024-07-01 03:00:07 -0700

    stillWorking

--- models/city.py
+++ models/city.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Defines the City class."""
+""" City Module for HBNB project """
 from models.base_model import Base
 from models.base_model import BaseModel
 from sqlalchemy import Column
@@ -7,17 +7,8 @@ from sqlalchemy import ForeignKey
 from sqlalchemy import String
 from sqlalchemy.orm import relationship
 
-
 class City(BaseModel, Base):
-    """Represents a city for a MySQL database.
-
-    Inherits from SQLAlchemy Base and links to the MySQL table cities.
-
-    Attributes:
-        __tablename__ (str): The name of the MySQL table to store Cities.
-        name (sqlalchemy String): The name of the City.
-        state_id (sqlalchemy String): The state id of the City.
-    """
+    """ The city class inherits from BaseModel and Base """
     __tablename__ = "cities"
     name = Column(String(128), nullable=False)
     state_id = Column(String(60), ForeignKey("states.id"), nullable=False)
