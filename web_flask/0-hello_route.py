02a3a890e3af1445e351f979675709fd91567053
Gadoskey <gadoski321@gmail.com>
2024-07-28 05:59:38 -0700

    ok

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,14 +1,18 @@
 #!/usr/bin/python3
-#0. Hello Flask!
-#A script that starts a Flask web application by GADOSKI
+"""0. Hello Flask!
+    A script that starts a Flask web application by GADOSKI
+"""
+
 from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route("/", strict_slashes=False)
 def hello_world():
-    # A function that returns Hello HBNB
+    """returns Hello HBNB!"""
     return "<p>Hello HBNB!</p>"
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
