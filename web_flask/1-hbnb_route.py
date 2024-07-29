32e69362a48fbb5bd6cea4cc959cf750aa8ea78f
Gadoskey <gadoski321@gmail.com>
2024-07-29 03:52:33 -0700

    ok

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""1. HBNB
+    A script that starts a Flask web application by GADOSKI
+"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def index():
+    """returns Hello HBNB!"""
+    return "Hello HBNB!"
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """returns Hello HBNB!"""
+    return "HBNB!"
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
