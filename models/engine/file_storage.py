11fcab882d8c84de21d5278146c198c8507e7fd0
Gadoskey <gadoski321@gmail.com>
2024-08-21 02:58:13 -0700

    corrected

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -20,7 +20,7 @@ class FileStorage:
             return {k: v for k, v in self.__objects
                     .items() if isinstance(v, cls)}
         return self.__objects
-    
+
     def delete(self, obj=None):
         """Delete a given object from __objects, if it exists."""
         try:
@@ -61,7 +61,7 @@ class FileStorage:
             with open(FileStorage.__file_path, 'r') as f:
                 temp = json.load(f)
                 for key, val in temp.items():
-                        self.all()[key] = classes[val['__class__']](**val)
+                    self.all()[key] = classes[val['__class__']](**val)
         except FileNotFoundError:
             pass
 
